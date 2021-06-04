646f2ef62f16f00c5a1af18a64b549ada7af52b0
Renerose Dimatulac <rsdimatulac@gmail.com>
2021-06-04 00:25:35 -0500

    Splash page with footer done, working on styling SignUp Modal

--- react-app/src/components/Footer.js
+++ react-app/src/components/Footer.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa";
 import "./Footer.css";
 
 const Footer = () => {
     return (
-        <div>
-            <h1>Footer</h1>
+        <div className="footer">
+            <div>© 2021 Flybook. No rights reserved.</div>
+            <div className="footer__links">
+
+                <a href="https://rsdimatulac.github.io/" style={{ textDecoration: "none" }}><p className="about__link">About the Developer</p></a>
+                <a href="https://github.com/rsdimatulac/Flybook">
+                    <FaGithub id="github" />
+                </a>
+                <a href="https://www.linkedin.com/in/renerosedimatulac/">
+                    <FaLinkedinIn id="linkedin" />
+                </a>
+            </div>
         </div>
     )
 }
--- react-app/src/components/SplashPage/Splash.js
+++ react-app/src/components/SplashPage/Splash.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import LoginForm from "../auth/LoginForm";
 import "./Splash.css";
 
 const Splash = () => {
     return (
-        <div>
-            <h1>Splash Page</h1>
+        <div className="splash">
+            <div className="splash__left">
+                <div className="splash__logo">
+                    <img src="https://theflybook.s3.amazonaws.com/flybook_logo.png" alt=""/>
+                </div>
+                <h1>Connect with pilots anywhere in the world</h1>
+            </div>
+            <div className="splash__right">
+                <LoginForm />
+            </div>
         </div>
     )
 }
