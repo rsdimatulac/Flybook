523c8778361b470e15bc48c2b5d3310a00c3be1a
Renerose Dimatulac <rsdimatulac@gmail.com>
2021-06-06 15:16:47 -0500

    Working on search

---
+++ react-app/src/components/Search.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Search.css";
+
+
+const Search = () => {
+    return (
+        <div className="search">
+            
+        </div>
+    )
+}
+
+export default Search;
