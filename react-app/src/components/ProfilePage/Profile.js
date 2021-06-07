45f30b3b3f1c7fdc9c0ca6b8e9fc1f83e113facf
Renerose Dimatulac <rsdimatulac@gmail.com>
2021-06-06 23:52:31 -0500

    Profila nav bar added and bottom layout

--- react-app/src/components/ProfilePage/Profile.js
+++ react-app/src/components/ProfilePage/Profile.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from "react-router-dom";
+import EditIcon from '@material-ui/icons/Edit';
 // import CameraAltIcon from '@material-ui/icons/CameraAlt';
 import { useSelector, useDispatch } from "react-redux";
 import { getAllUsers } from "../../store/users";
@@ -30,7 +31,7 @@ const Profile = ({ currentUser }) => {
     const handleBioSubmit = async (e) => {
         e.preventDefault();
         await dispatch(editUserProfile(currentUser?.id, { type: "bio", data: newBio }));
-        
+
         setShowBioInput(false);
         setNewBio("");
     };
@@ -45,18 +46,33 @@ const Profile = ({ currentUser }) => {
             </div>
             <div className="profile__name">
                 <h1>{theUser?.firstname} {theUser?.lastname}</h1>
-                {showBioInput 
-                ? <form onSubmit={handleBioSubmit}>
-                    <textarea className="bio__textarea" value={newBio} onChange={(e) => setNewBio(e.target.value)}/>
-                    <p>{101 - newBio.length} characters remaining</p>
-                    <div className="bio__buttons">
-                        <div onClick={() => setShowBioInput(prevState => !prevState)}>Cancel</div>
-                        <button disabled={(101 - newBio.length) < 0 ? true : false} type="submit">Save</button>
-                    </div>
-                </form>
-                    
-                : <h2>{theUser?.bio}</h2>}
+                {showBioInput
+                    ? <form onSubmit={handleBioSubmit}>
+                        <textarea className="bio__textarea" value={newBio} onChange={(e) => setNewBio(e.target.value)} />
+                        <p>{101 - newBio.length} characters remaining</p>
+                        <div className="bio__buttons">
+                            <div onClick={() => setShowBioInput(prevState => !prevState)}>Cancel</div>
+                            <button disabled={(101 - newBio.length) < 0 ? true : false} type="submit">Save</button>
+                        </div>
+                    </form>
+                    : <h2>{theUser?.bio}</h2>}
                 {showBioInput === false && currentUser.id === Number(userId) && <div className="editBio__button" onClick={handleEditBio(theUser?.bio)}>Edit</div>}
+            </div>
+            <div className="profile__nav">
+                <div className="profileNav__links">
+                    <div className={`profileNav__links--active`}><p>Timeline</p></div>
+                    <div><p>About</p></div>
+                    <div><p>Friends</p></div>
+                    <div><p>Photos</p></div>
+                </div>
+                <div className="profileNav__edit">
+                    <EditIcon />
+                    <p>Edit Profile</p>
+                </div>
+            </div>
+            <div className="profile__bottom">
+                <div className="bottom__left">Left side</div>
+                <div className="bottom__right">Right side</div>
             </div>
         </div>
     )
