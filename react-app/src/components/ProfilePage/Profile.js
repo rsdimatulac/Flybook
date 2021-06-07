e09a8b4415517df60b41357b0f17fcca555db174
Renerose Dimatulac <rsdimatulac@gmail.com>
2021-06-06 22:29:34 -0500

    Building Profile. Edit Bio working and updating

--- react-app/src/components/ProfilePage/Profile.js
+++ react-app/src/components/ProfilePage/Profile.js
@@ -1,11 +1,63 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from "react-router-dom";
+// import CameraAltIcon from '@material-ui/icons/CameraAlt';
+import { useSelector, useDispatch } from "react-redux";
+import { getAllUsers } from "../../store/users";
+import { editUserProfile, getUser } from "../../store/user";
 import "./Profile.css";
 
 
-const Profile = () => {
+const Profile = ({ currentUser }) => {
+    const { userId } = useParams();
+    const user = useSelector(state => state.user);
+    const theUser = user[userId];
+    const [showBioInput, setShowBioInput] = useState(false);
+    const [newBio, setNewBio] = useState("");
+    const dispatch = useDispatch();
+
+
+    useEffect(() => {
+        dispatch(getAllUsers());
+        dispatch(getUser(Number(userId)))
+    }, [dispatch, userId])
+
+
+    const handleEditBio = (bio) => () => {
+        setShowBioInput(true);
+        setNewBio(bio ? bio : "");
+    }
+
+    const handleBioSubmit = async (e) => {
+        e.preventDefault();
+        await dispatch(editUserProfile(currentUser?.id, { type: "bio", data: newBio }));
+        
+        setShowBioInput(false);
+        setNewBio("");
+    };
+
     return (
-        <div>
-            <h1>Profile</h1>
+        <div className="profile">
+            <div className="profile__top">
+                <div className="cover__photo" style={{ backgroundImage: `url(${theUser?.cover_src})` }}></div>
+                <div className="profile__avatar">
+                    <img src={theUser?.profile_src} alt="" />
+                </div>
+            </div>
+            <div className="profile__name">
+                <h1>{theUser?.firstname} {theUser?.lastname}</h1>
+                {showBioInput 
+                ? <form onSubmit={handleBioSubmit}>
+                    <textarea className="bio__textarea" value={newBio} onChange={(e) => setNewBio(e.target.value)}/>
+                    <p>{101 - newBio.length} characters remaining</p>
+                    <div className="bio__buttons">
+                        <div onClick={() => setShowBioInput(prevState => !prevState)}>Cancel</div>
+                        <button disabled={(101 - newBio.length) < 0 ? true : false} type="submit">Save</button>
+                    </div>
+                </form>
+                    
+                : <h2>{theUser?.bio}</h2>}
+                {showBioInput === false && currentUser.id === Number(userId) && <div className="editBio__button" onClick={handleEditBio(theUser?.bio)}>Edit</div>}
+            </div>
         </div>
     )
 }
--- react-app/src/components/Search.js
+++ react-app/src/components/Search.js
@@ -1,29 +1,35 @@
 import React from 'react';
 import { useHistory } from "react-router-dom";
 import { Avatar } from '@material-ui/core';
+import useConsumeContext from "../context/ModalContext";
 import "./Search.css";
 
 
-const Search = ({ setShowSearch, searchResults, currentUser, setSearchResults, setSearchInput }) => {
+const Search = ({currentUser}) => {
+    const { setSearchInput, searchResults, setSearchResults, setShowSearch } = useConsumeContext();
+
     const history = useHistory();
 
     const handleUserClick = (userID) => () => {
-        history.push(`/users/${userID}`);
-        setSearchResults([]);
-        setSearchInput("");
-        setShowSearch(false);
+        if (userID !== undefined) {
+            history.push(`/users/${userID}`);
+            setSearchResults([]);
+            setSearchInput("");
+            setShowSearch(false);
+        } // TEST
+
     };
 
     return (
         <div className="search">
             {searchResults.length === 0 ? <div className="no__results">No results</div> : searchResults.map(user => (
-                <div className={`${user.id} search__results`} key={user.id} onClick={handleUserClick(user.id)}>
+                <div className={`${user?.id} search__results`} key={user?.id} onClick={handleUserClick(user?.id)}>
                     <div className="searchResults__avatar">
                         <Avatar src={user.profile_src}/>
                     </div>
                     <div className="searchResults__name">
                         <h3>{user.firstname} {user.lastname}</h3>
-                        {currentUser.id === user.id ? <p>You</p> : ""}
+                        {currentUser?.id === user?.id ? <p>You</p> : ""}
                     </div>
                 </div>
             ))}
