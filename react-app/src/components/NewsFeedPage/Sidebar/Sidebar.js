4d7f461d95d38b8e203db8b5954932965c26fe62
Renerose Dimatulac <rsdimatulac@gmail.com>
2021-06-10 11:40:17 -0500

    Added flight video modal

--- react-app/src/components/NewsFeedPage/Sidebar/Sidebar.js
+++ react-app/src/components/NewsFeedPage/Sidebar/Sidebar.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { NavLink } from "react-router-dom";
 import SidebarRow from "./SidebarRow";
+import VideoModal from "../VideoModal";
+import useConsumeContext from "../../../context/ModalContext";
 import "./Sidebar.css";
 
 
 const Sidebar = ({ user }) => {
+    const { showVideoModal, setShowVideoModal } = useConsumeContext();
+
     return (
         <div className="sidebar">
             <NavLink to={`/users/${user?.id}`} style={{ textDecoration: "none", color: "inherit" }}>
@@ -14,7 +18,8 @@ const Sidebar = ({ user }) => {
                 <SidebarRow src={"https://static.xx.fbcdn.net/rsrc.php/v3/yi/r/FZK_jEWapM0.png"} title="COVID-19 Information Center" />
             </a>
             <SidebarRow src={"https://static.xx.fbcdn.net/rsrc.php/v3/yj/r/tSXYIzZlfrS.png"} title="Friends" />
-            <SidebarRow src={"https://static.xx.fbcdn.net/rsrc.php/v3/y-/r/FhOLTyUFKwf.png"} title="Watch" />
+            <SidebarRow onVideoClick={() => setShowVideoModal(prevState => !prevState)} src={"https://static.xx.fbcdn.net/rsrc.php/v3/y-/r/FhOLTyUFKwf.png"} title="Watch" />
+            {showVideoModal && <VideoModal />}
         </div>
     )
 }
