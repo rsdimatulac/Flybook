5e0e3d55bc9d20435c085f7be737355dfadb8b68
Renerose Dimatulac <rsdimatulac@gmail.com>
2021-06-12 00:23:58 -0500

    Fixing heroku css issues

--- react-app/src/components/NewsFeedPage/Feed/StoryReel.js
+++ react-app/src/components/NewsFeedPage/Feed/StoryReel.js
@@ -5,7 +5,7 @@ import "./StoryReel.css";
 
 const StoryReel = ({ user }) => {
     const friends = Object.values(user?.friends);
-    const friendsPost = friends.map(friend => friend.posts !== undefined && Object.values(friend.posts).find(post => (post.photo_src !== "" && post.photo_src !== null))).slice(0, 5); // limit to 5 only
+    const friendsPost = friends.map(friend => friend?.posts !== undefined && Object.values(friend?.posts).find(post => (post?.photo_src !== "" && post?.photo_src !== null))).slice(0, 5); // limit to 5 only
     const history = useHistory();
     
     const goToProfile = (friendId) => () => {
@@ -18,8 +18,8 @@ const StoryReel = ({ user }) => {
                 (post === undefined 
                 ? "" 
                 : <div onClick={goToProfile(friends[index]?.id)} key={friends[index]?.id} className="story" style={{ backgroundImage: `url(${post?.photo_src})` }}>
-                    <Avatar src={friends[index].profile_src} className="story__avatar"/>
-                    <h4>{`${friends[index].firstname} ${friends[index].lastname}`}</h4>
+                    <Avatar src={friends[index]?.profile_src} className="story__avatar"/>
+                    <h4>{`${friends[index]?.firstname} ${friends[index]?.lastname}`}</h4>
                 </div>)
             )}
         </div>
